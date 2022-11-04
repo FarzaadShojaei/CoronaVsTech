f1591b5adc7310c4d76a1bd0653c3720a4fa59db
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-11-05 00:11:16 +0330

    Completing Admin Panel

---
+++ api/coronavstech/companies/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from api.coronavstech.companies.models import Company
+
+
+# Register your models here.
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass 
+
